8597f09387b005ab999273ef3faaca19980a6323
Chris Ye <chye0001@kea.stud.dk>
2025-01-31 10:38:55 +0100

    commited variables.js

---
+++ miscellaneous/variables.js
@@ -0,0 +1,55 @@
+console.log("Hello world")
+
+// const is NOT a constant
+// that means that const CAN have its value changed
+// const is a constant in the DECLARATION
+// const MUST be declared as it is initialized, i.e. you MUST declare/initilize it with a value.
+
+// TODO What is the difference between declaration and initilizetion
+
+const me = {};
+me.name = "Bo"
+console.log(me)
+
+const hobbies = ["coding"];
+hobbies.pop();
+hobbies.push("chilling", "eating");
+
+console.log(hobbies);
+// ^^
+// We have demostrated that const can have its values changed, but the delcatation IS constant and cant be changed.
+
+
+
+
+
+
+// jsObject is not the same as JSON.
+const jsObject = {
+
+}
+
+
+
+
+
+
+// "" or '' or `` = string literals
+// `` = template strings
+// ${} syntax = string interpolation
+
+//String interpolation f.eks: ${2 + 2}
+//String literal f.eks: ` string, html and stuff inside backtick ${2 + 2} `
+
+
+
+
+
+
+const assignmentDescription = "...and the value is..."
+const value = 4;
+
+// don't use plus "+" in console.log use "," instead to avoid type coercion. A.K.A, us "," so it wont change the datatype for value to String.
+// but if you use plus "+" it would change the datatype to a String. We dont want this as the console.log() is made for us devs to debug. We want to see the datatype
+console.log(assignmentDescription, value)
+console.log(assignmentDescription + " " + value)
