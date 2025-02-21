62e4e4f7ef6c05ed57ff14aff4e51841fe40dba7
Chris Ye <chye0001@kea.stud.dk>
2025-02-21 09:32:30 +0100

    Created more effective delete method. Also added pathc

--- 03.Finger_Bones_REST_API/app.js
+++ 03.Finger_Bones_REST_API/app.js
@@ -13,7 +13,7 @@ let fingerBones = [
   },
 ];
 
-let id = 2;
+let nextId = 2;
 
 // route / get handler
 app.get("/fingerbones", (req, res) => {
@@ -33,11 +33,17 @@ app.get("/fingerbones/:id", (req, res) => {
   }
 });
 
+// postfix increment
+// value++;
+
+// prefix increment
+// ++value;
+
 app.post("/fingerbones", (req, res) => {
   const postReq = req.body;
 
   const newFingerbone = {
-    id: ++id,
+    id: nextId++,
     artistName: postReq.artistName,
   };
 
@@ -66,69 +72,57 @@ app.put("/fingerbones/:id", (req, res) => {
   res.send({ data: updatedFingerBone });
 });
 
+app.patch("/fingerbones/:id", (req, res) => {
+  const id = NUmber(req.params.id);
+  const foundFingerboneIndex = fingerBones.findIndex( (fingerbone) => fingerbone.id === id );
+
+  if (foundFingerboneIndex === -1) {
+    res.status(404).send( {error: `Fingerbone with id: ${id} does not exist in the database`} );
+    return;
+
+  } else {
+    const fingerboneToEdit = fingerBones[foundFingerboneIndex];
+    const updatedFingerBone = { ...fingerboneToEdit, ...req.body, id: id}
+    fingerBones[foundFingerboneIndex] = updatedFingerBone;
+
+    res.send( {data: updatedFingerBone} )
+  }
+
+})
+
+
+// Ineffective method
+// loops the entire array regardles of found.
+// app.delete("/fingerbones/:id", (req, res) => {
+//   const id = Number(req.params.id);
+
+//   const doesExist = doesIdExist(id);
+//   if (!doesExist) {
+//     res.status(400).send({ error: `ID: ${id} does not exist` });
+//     return;
+//   }
+
+//   fingerBones = fingerBones.filter((fingerbone) => fingerbone.id !== id);
+//   res.send({ success: `Fingerbone with ID: ${id} was removed` });
+// });
+
+// More effective delete method
+// only loops until index is found.
 app.delete("/fingerbones/:id", (req, res) => {
   const id = Number(req.params.id);
 
-  const doesExist = doesIdExist(id);
-  if (!doesExist) {
-    res.status(400).send({ error: `ID: ${id} does not exist` });
-    return;
+  const foundIndex = fingerBones.findIndex( (fingerbone) => fingerbone.id === id );
+  if (foundIndex === -1) {
+    res.status(404).send( {error: `Fingerbone with id: ${id} does not exist in the database`} )
+
+  } else {
+    fingerBones.splice(foundIndex);
+    res.status(204).send();
   }
+})
 
-  fingerBones = fingerBones.filter((fingerbone) => fingerbone.id !== id);
-  res.send({ success: `Fingerbone with ID: ${id} was removed` });
-});
 
-//validation functions
 
-// function validateRequest(req, res) {
-//   const reqBody = req.body;
-//   const invalidFields = validateFields(reqBody, res);
-//   if (invalidFields) {
-//     return true;
-//   }
-
-//   const id = Number(reqBody.id);
-//   const invalidIdType = validateIdIsNumber(id, res);
-//   if (invalidIdType) {
-//     return true;
-//   }
-
-//   const invalidIdSize = validateIdLargerThanZero(id, res);
-//   if (invalidIdSize) {
-//     return true;
-//   }
-
-//   const invalidId = doesIdExist(id, res);
-//   if (invalidId) {
-//     return true;
-//   }
-
-//   return false;
-// }
-
-// function validateFields(reqBody, res) {
-//   if (reqBody.id === undefined || reqBody.artistName === undefined) {
-//     res.status(400).send({
-//       error: `The post request most contain the following fields: id and artistName`,
-//     });
-//     return true;
-//   }
-// }
-
-// function validateIdIsNumber(id, res) {
-//   if (isNaN(id)) {
-//     res.status(400).send({ error: `ID most be a number` });
-//     return true;
-//   }
-// }
-
-// function validateIdLargerThanZero(id, res) {
-//   if (id <= 0) {
-//     res.status(400).send({ error: `ID most be larger than 0` });
-//     return true;
-//   }
-// }
 
 function doesIdExist(id) {
   const isFoundId = fingerBones.some((fingerbone) => fingerbone.id === id);
@@ -145,4 +139,12 @@ function doesIdExist(id) {
 // 5XX - Server error
 
 const PORT = 8080;
-app.listen(PORT);
+app.listen(PORT, (error) => {
+
+  if(error) {
+    console.log(`Error startung the server`, error);
+    return;
+  }
+
+  console.log(`Server is running on port`, PORT)
+});
