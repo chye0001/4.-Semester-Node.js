7dfd1298a9befc18a5b28510dcae6258ef499bef
Chris Ye <chye0001@kea.stud.dk>
2025-03-21 11:45:56 +0100

    Worked with props

---
+++ 07.First_Svelte_Project/src/components/Messages/FirstMessage.svelte
@@ -0,0 +1,5 @@
+<script>
+    let { firstMessage = "No messages yet." } = $props();
+</script>
+
+<h1>First message: {firstMessage}</h1>
--- 07.First_Svelte_Project/src/components/Messages/Messages.svelte
+++ 07.First_Svelte_Project/src/components/Messages/Messages.svelte
@@ -1,5 +1,7 @@
 <script>
-    let messageList = $state(["asdasda", "sdfsdfsf", 12]);
+  import FirstMessage from "./FirstMessage.svelte";
+
+    let messageList = $state([]);
     let messageInput = $state("");
 
     function addMessage(){
@@ -12,3 +14,5 @@
 <input bind:value={messageInput} placeholder="Type your message">
 <button onclick={addMessage}>Submit</button>
 <p>{messageList}</p>
+
+<FirstMessage firstMessage={messageList[0]} />
