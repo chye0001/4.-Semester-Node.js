956dc9639c34361f21268fe5fb5ef777e3dd3e15
Chris Ye <chye0001@kea.stud.dk>
2025-03-21 10:57:02 +0100

    created 2 componentes Counter and Messages

---
+++ 07.First_Svelte_Project/src/components/Counter/Counter.svelte
@@ -0,0 +1,14 @@
+<script>
+    let counter = 0;
+</script>
+
+
+<h3 id="counter">{counter}</h3>
+<button onclick={() => (counter--)}>Decrement</button>
+<button onclick={() => (counter++)}>Increment</button>
+
+<style>
+    #counter{
+        color: gainsboro;
+    }
+</style>
---
+++ 07.First_Svelte_Project/src/components/Messages/Messages.svelte
@@ -0,0 +1,14 @@
+<script>
+    let messageList = $state(["asdasda", "sdfsdfsf", 12]);
+    let messageInput = $state("");
+
+    function addMessage(){
+        messageList.push(messageInput);
+        messageInput = "";
+    }
+</script>
+
+<h2>Messages</h2>
+<input bind:value={messageInput} placeholder="Type your message">
+<button onclick={addMessage}>Submit</button>
+<p>{messageList}</p>
